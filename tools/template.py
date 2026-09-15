a36525fb28259b410dc7df7673a7bad7334be797
unac_stack <zchao_chen@126.com>
2026-09-15 11:11:26 +0800

    Add CANN 9.2.0-beta.2 images

--- tools/template.py
+++ tools/template.py
@@ -26,7 +26,8 @@ ALPHA_DICT = {
     "9.0.0-beta.2": "9.0.T511",
     "9.1.0-beta.1": "9.1.T1",
     "9.1.0-beta.3": "9.1.T6",
-    "9.2.0-beta.1": "9.2.T1"
+    "9.2.0-beta.1": "9.2.T1",
+    "9.2.0-beta.2": "9.2.T3"
 }
 
 env = Environment(loader=FileSystemLoader('tools/template'), trim_blocks=True, lstrip_blocks=True)
